5b837f4408434dd9a77b0870e9c59289882c1a12
skhimsara <skhimsara@gmail.com>
2016-09-25 13:53:05 -0700

    smile detection

--- face.py
+++ face.py
@@ -2,8 +2,11 @@ import cv2
 import sys
 
 cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier("C:\Anaconda2\Library\share\OpenCV\haarcascades\haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier("C:\Anaconda2\Library\share\OpenCV\haarcascades\haarcascade_eye.xml")
+faceCascade = cv2.CascadeClassifier("/Users/sidnpoo/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("/Users/sidnpoo/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_eye.xml")
+smilePath = "/Users/sidnpoo/Downloads/opencv-3.1.0/data/haarcascades/haarcascade_smile.xml"
+smileCascade = cv2.CascadeClassifier(smilePath)
+
 
 video_capture = cv2.VideoCapture(0)
 
@@ -16,7 +19,7 @@ while True:
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
-        minNeighbors=10,
+        minNeighbors=20,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
@@ -28,11 +31,26 @@ while True:
         roi_color = frame[y:y+h, x:x+w]  #part of face inside the rectangle
         eyes = eye_cascade.detectMultiScale(roi_gray,
                                                 scaleFactor=1.1,
-                                                minNeighbors=10,
+                                                minNeighbors=20,
                                                 minSize=(30, 30),
                                                 flags=cv2.CASCADE_SCALE_IMAGE) #look only in that rectangle
         for (ex,ey,ew,eh) in eyes: 
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+
+	smile = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.1,
+            minNeighbors=22,
+            minSize=(25, 25),
+            flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+        # Set region of interest for smiles
+	for (x, y, w, h) in smile:
+            print "Found", len(smile), "smiles!"
+            cv2.rectangle(roi_color, (x, y), (x+w, y+h), (0, 255, 0), 1)
+            #print "!!!!!!!!!!!!!!!!!"
+
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
